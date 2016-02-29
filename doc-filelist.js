d81f6ba00481aad33d2d59387ba56195170a855c
Alinex <info@alinex.de>
2016-02-29 14:28:37 +0100

    Updated documentation

--- doc-filelist.js
+++ doc-filelist.js
@@ -1,1 +1,1 @@
-var tree={"files":[".travis.yml","Changelog.md","README.md"],"dirs":{"src":{"files":["check.coffee","configSchema.coffee","helper.coffee","index.coffee","spawn.coffee","ssh.coffee"]},"var":{"dirs":{"src":{"dirs":{"config":{"files":["exec.yml"]}}}}}}};
+var tree={"files":["Changelog.md","README.md","coffeelint.json","package.json"],"dirs":{"src":{"files":["check.coffee","configSchema.coffee","helper.coffee","index.coffee","spawn.coffee","ssh.coffee"]}}};
